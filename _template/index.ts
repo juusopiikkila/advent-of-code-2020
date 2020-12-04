7dfa1bba04cc0515d4c40af74bf379f2ad9bf88a
Juuso Piikkilä <juuso@meiko.fi>
2020-12-04 09:16:07 +0200

    update template index

--- _template/index.ts
+++ _template/index.ts
@@ -12,16 +12,15 @@ function part2(data: string[]): number {
 
 try {
     readFileToArray('./1/input.txt').then((data) => {
+        const testData = [
+
+        ];
+
         // strictEqual(test(12), 2);
-        // strictEqual(test(14), 2);
-        // strictEqual(test(1969), 654);
-        // strictEqual(test(100756), 33583);
 
         console.log('Part 1', part1(data));
 
         // strictEqual(test(14), 2);
-        // strictEqual(test(1969), 966);
-        // strictEqual(test(100756), 50346);
 
         console.log('Part 2', part2(data));
     });
